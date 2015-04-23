16ec064b51348a5e9b3cf0cdc198847770f265ec
Nicolas Morisset <Nicolas.Morisset@unige.ch>
2015-04-23 14:03:32 +0000

    - Added more checks on the parameters - Updated the unit test - Use of the second constructor when no deltaZ column in the catalog [Redmine Issue #7516]
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/PhosphorosCore/trunk@7481 463500fe-d6be-4184-9b20-47717e75ecac

--- PhzConfiguration/src/lib/SpectroscopicRedshiftCatalogConfiguration.cpp
+++ PhzConfiguration/src/lib/SpectroscopicRedshiftCatalogConfiguration.cpp
@@ -25,7 +25,8 @@ static Elements::Logging logger = Elements::Logging::getLogger(
 
 po::options_description SpectroscopicRedshiftCatalogConfiguration::getProgramOptions() {
   po::options_description options = CatalogConfiguration::getProgramOptions();
-  options.add_options()("spec-z-column-name", po::value<std::string>(),
+  options.add_options()(
+      "spec-z-column-name", po::value<std::string>(),
       "The name of the column representing the spectroscopic redshift")(
       "spec-z-column-index", po::value<int>(),
       "The index of the column representing the spectroscopic redshift")(
@@ -39,43 +40,55 @@ po::options_description SpectroscopicRedshiftCatalogConfiguration::getProgramOpt
 SpectroscopicRedshiftCatalogConfiguration::SpectroscopicRedshiftCatalogConfiguration(
     const std::map<std::string, po::variable_value>& options) :
     CatalogConfiguration(options) {
-  if (options.find("spec-z-column-name")!=options.end()
-      && options.find("spec-z-column-index")!=options.end()) {
-    logger.error(
-        "Found both spec-z column-name and spec-z-column-index options");
-    throw Elements::Exception(
-        "Options spec-z-column-name and spec-z-column-index are mutually exclusive");
+
+  auto options_local = options;
+
+  // Both options for spec_z not allowed
+  if (!options_local["spec-z-column-name"].empty() &&
+      !options_local["spec-z-column-index"].empty()) {
+     logger.error("Found both spec-z-column-name and spec-z-column-index options");
+     throw Elements::Exception("Options spec-z-column-name and spec-z-column-index are mutually exclusive");
+  }
+
+  // Both options for spec-z-err not allowed
+  if (!options_local["spec-z-err-column-name"].empty() &&
+      !options_local["spec-z-err-column-index"].empty()) {
+     logger.error("Found both spec-z-err-column-name and spec-z-err-column-index options");
+     throw Elements::Exception("Options spec-z-err-column-name and spec-z-err-column-index are mutually exclusive");
   }
 
   string column_name="";
-  if (options.find("spec-z-column-name")==options.end()) {
-    column_name = getAsTable().getColumnInfo()->getName(
-        boost::any_cast<int>(options.find("spec-z-column-index")->second.value())-1);
-  } else{
-    column_name = boost::any_cast<string>(options.find("spec-z-column-name")->second.value());
-  }
+  // Get column name
+  if (options_local["spec-z-column-name"].empty()) {
+      column_name = getAsTable().getColumnInfo()->getName(
+      boost::any_cast<int>(options.find("spec-z-column-index")->second.value())-1);
+    }
+  else {
+      column_name = boost::any_cast<string>(options.find("spec-z-column-name")->second.value());
+    }
 
-  if (options.find("spec-z-err-column-name")!=options.end()
-      && options.find("spec-z-err-column-index")!=options.end()) {
-    logger.error(
-        "Found both spec-err-z column-name and spec-z-err-column-index options");
-    throw Elements::Exception(
-        "Options spec-z-err-column-name and spec-z-err-column-index are mutually exclusive");
+  // Get error column
+  string error_column_name  = "";
+  bool   error_column_found = false;
+  if (!options_local["spec-z-err-column-name"].empty()) {
+    error_column_name = boost::any_cast<string>(options_local.find("spec-z-err-column-name")->second.value());
+    error_column_found = true;
   }
-
-  string error_column_name="";
-   if (options.find("spec-z-err-column-name")==options.end() && options.find("spec-z-err-column-index")!=options.end()) {
+  else if (!options_local["spec-z-err-column-index"].empty()){
     error_column_name = getAsTable().getColumnInfo()->getName(
-        boost::any_cast<int>(options.find("spec-z-err-column-index")->second.value())-1);
-  } else if (options.find("spec-z-err-column-index")!=options.end()) {
-    error_column_name = boost::any_cast<string>(options.find("spec-z-err-column-name")->second.value());
+    boost::any_cast<int>(options_local.find("spec-z-err-column-index")->second.value())-1);
+    error_column_found = true;
   }
+
   // Add the row handler to parse the spectroscopic redshift
-std::shared_ptr<SourceCatalog::AttributeFromRow> handler_ptr {new SourceCatalog::SpectroscopicRedshiftAttributeFromRow {getAsTable().getColumnInfo(), column_name,error_column_name}
+  std::shared_ptr<SourceCatalog::AttributeFromRow> handler_ptr{};
+  if (error_column_found) {
+    handler_ptr.reset( new SourceCatalog::SpectroscopicRedshiftAttributeFromRow { getAsTable().getColumnInfo(), column_name, error_column_name } );
+  } else {
+    handler_ptr.reset( new SourceCatalog::SpectroscopicRedshiftAttributeFromRow { getAsTable().getColumnInfo(), column_name } );
   }
-  ;
   addAttributeHandler (std::move(handler_ptr));
-  }
+}
 
   } // end of namespace PhzConfiguration
 } // end of namespace Euclid
--- PhzConfiguration/tests/src/SpectroscopicRedshiftCatalogConfiguration_test.cpp
+++ PhzConfiguration/tests/src/SpectroscopicRedshiftCatalogConfiguration_test.cpp
@@ -36,13 +36,14 @@ struct SpectroscopicRedshiftCatalogConfiguration_Fixture {
 
   std::map<std::string, po::variable_value> options_map_min_working;
   std::map<std::string, po::variable_value> options_map_min_working_2;
-  std::map<std::string, po::variable_value> options_map_2_z;
-  std::map<std::string, po::variable_value> options_map_1_deltaZ_1;
-  std::map<std::string, po::variable_value> options_map_1_deltaZ_2;
-  std::map<std::string, po::variable_value> options_map_2_deltaZ;
+  std::map<std::string, po::variable_value> options_map_z_index;
+  std::map<std::string, po::variable_value> options_map_z_deltaZ;
+  std::map<std::string, po::variable_value> options_map_z_deltaZ_index;
+  std::map<std::string, po::variable_value> options_map_z_deltaZ_index_plus_name;
 
   Elements::TempDir temp_dir;
-  std::string base_directory { temp_dir.path().native() + "/euclid_test/" };
+  //zzz std::string base_directory { temp_dir.path().native() + "/euclid_test/" };
+  std::string base_directory { "/tmp/euclid_test/" };
   std::string ascii_file_name { base_directory + "test_SpecZ_Catalog.txt" };
   SpectroscopicRedshiftCatalogConfiguration_Fixture() {
     makeDirectory(base_directory);
@@ -58,24 +59,23 @@ struct SpectroscopicRedshiftCatalogConfiguration_Fixture {
     options_map_min_working_2["input-catalog-file"].value() = ascii_file_name;
     options_map_min_working_2["spec-z-column-index"].value() = 2;
 
-    options_map_2_z["input-catalog-file"].value() = ascii_file_name;
-    options_map_2_z["spec-z-column-name"].value() = std::string("Z");
-    options_map_2_z["spec-z-column-index"].value() = 2;
+    options_map_z_index["input-catalog-file"].value() = ascii_file_name;
+    options_map_z_index["spec-z-column-name"].value() = std::string("Z");
+    options_map_z_index["spec-z-column-index"].value() = 2;
 
-    options_map_1_deltaZ_1["input-catalog-file"].value() = ascii_file_name;
-    options_map_1_deltaZ_1["spec-z-column-name"].value() = std::string("Z");
-    options_map_1_deltaZ_1["spec-z-err-column-name"].value() = std::string(
+    options_map_z_deltaZ["input-catalog-file"].value() = ascii_file_name;
+    options_map_z_deltaZ["spec-z-column-name"].value() = std::string("Z");
+    options_map_z_deltaZ["spec-z-err-column-name"].value() = std::string(
         "DeltaZ");
 
-    options_map_1_deltaZ_2["input-catalog-file"].value() = ascii_file_name;
-    options_map_1_deltaZ_2["spec-z-column-name"].value() = std::string("Z");
-    options_map_1_deltaZ_2["spec-z-err-column-index"].value() = 3;
+    options_map_z_deltaZ_index["input-catalog-file"].value() = ascii_file_name;
+    options_map_z_deltaZ_index["spec-z-column-name"].value() = std::string("Z");
+    options_map_z_deltaZ_index["spec-z-err-column-index"].value() = 3;
 
-    options_map_2_deltaZ["input-catalog-file"].value() = ascii_file_name;
-    options_map_2_deltaZ["spec-z-column-name"].value() = std::string("Z");
-    options_map_2_deltaZ["spec-z-err-column-name"].value() = std::string(
-        "DeltaZ");
-    options_map_2_deltaZ["spec-z-err-column-index"].value() = 3;
+    options_map_z_deltaZ_index_plus_name["input-catalog-file"].value() = ascii_file_name;
+    options_map_z_deltaZ_index_plus_name["spec-z-column-name"].value() = std::string("Z");
+    options_map_z_deltaZ_index_plus_name["spec-z-err-column-name"].value() = std::string("DeltaZ");
+    options_map_z_deltaZ_index_plus_name["spec-z-err-column-index"].value() = 3;
   }
 
   ~SpectroscopicRedshiftCatalogConfiguration_Fixture() {
@@ -88,7 +88,7 @@ struct SpectroscopicRedshiftCatalogConfiguration_Fixture {
 BOOST_AUTO_TEST_SUITE (SpectroscopicRedshiftCatalogConfiguration_test)
 
 //-----------------------------------------------------------------------------
-// Test the getProgramOptions function for sed-root-path
+// Test the getProgramOptions function
 //-----------------------------------------------------------------------------
 
 BOOST_FIXTURE_TEST_CASE(getProgramOptions_function_test, SpectroscopicRedshiftCatalogConfiguration_Fixture) {
@@ -97,8 +97,7 @@ BOOST_FIXTURE_TEST_CASE(getProgramOptions_function_test, SpectroscopicRedshiftCa
   BOOST_TEST_MESSAGE("--> Testing the getProgramOptions function");
   BOOST_TEST_MESSAGE(" ");
 
-  auto option_desc =
-      Euclid::PhzConfiguration::SpectroscopicRedshiftCatalogConfiguration::getProgramOptions();
+  auto option_desc = Euclid::PhzConfiguration::SpectroscopicRedshiftCatalogConfiguration::getProgramOptions();
   const boost::program_options::option_description* desc { };
 
   desc = option_desc.find_nothrow("spec-z-column-name", false);
@@ -109,43 +108,93 @@ BOOST_FIXTURE_TEST_CASE(getProgramOptions_function_test, SpectroscopicRedshiftCa
   BOOST_CHECK(desc != nullptr);
   desc = option_desc.find_nothrow("spec-z-err-column-index", false);
   BOOST_CHECK(desc != nullptr);
+
+}
+
+BOOST_FIXTURE_TEST_CASE(SpectroscopicRedshiftCatalogConfiguration_constructor_z_plus_deltaz_test, SpectroscopicRedshiftCatalogConfiguration_Fixture) {
+
+  BOOST_TEST_MESSAGE(" ");
+  BOOST_TEST_MESSAGE("--> Testing the constructor, Z and deltaZ columns");
+  BOOST_TEST_MESSAGE(" ");
+
+  cf::SpectroscopicRedshiftCatalogConfiguration fconf(options_map_z_deltaZ);
+  auto catalog = fconf.getCatalog();
+  auto photo_z1 =catalog.find(1)->getAttribute<Euclid::SourceCatalog::SpectroscopicRedshift>();
+  auto photo_z2 =catalog.find(2)->getAttribute<Euclid::SourceCatalog::SpectroscopicRedshift>();
+
+  BOOST_CHECK(Elements::isEqual(0.01,photo_z1->getError()));
+  BOOST_CHECK(Elements::isEqual(0.02,photo_z2->getError()));
+
+}
+
+BOOST_FIXTURE_TEST_CASE(SpectroscopicRedshiftCatalogConfiguration_constructor_z_plus_deltaz_index_test, SpectroscopicRedshiftCatalogConfiguration_Fixture) {
+
+  BOOST_TEST_MESSAGE(" ");
+  BOOST_TEST_MESSAGE("--> Testing the constructor, Z and deltaZ (as index) columns");
+  BOOST_TEST_MESSAGE(" ");
+
+  cf::SpectroscopicRedshiftCatalogConfiguration fconf(options_map_z_deltaZ_index);
+  auto catalog = fconf.getCatalog();
+  auto photo_z1 =catalog.find(1)->getAttribute<Euclid::SourceCatalog::SpectroscopicRedshift>();
+  auto photo_z2 =catalog.find(2)->getAttribute<Euclid::SourceCatalog::SpectroscopicRedshift>();
+
+  BOOST_CHECK(Elements::isEqual(0.01,photo_z1->getError()));
+  BOOST_CHECK(Elements::isEqual(0.02,photo_z2->getError()));
+
 }
 
 BOOST_FIXTURE_TEST_CASE(SpectroscopicRedshiftCatalogConfiguration_constructor_test, SpectroscopicRedshiftCatalogConfiguration_Fixture) {
 
   BOOST_TEST_MESSAGE(" ");
-  BOOST_TEST_MESSAGE("--> Testing the constructor");
+  BOOST_TEST_MESSAGE("--> Testing the constructor, Z column but no error column");
   BOOST_TEST_MESSAGE(" ");
 
-  // Nominal use-cases without error
   cf::SpectroscopicRedshiftCatalogConfiguration fconf(options_map_min_working);
-  fconf.getCatalog();
-  fconf = cf::SpectroscopicRedshiftCatalogConfiguration(options_map_min_working_2);
-  auto catalog_without_err = fconf.getCatalog();
-  //check the error
-  auto photo_z =catalog_without_err.find(1)->getAttribute<Euclid::SourceCatalog::SpectroscopicRedshift>();
-  BOOST_CHECK(Elements::isEqual(0.,photo_z->getError()));
+  auto catalog = fconf.getCatalog();
+  auto photo_z1 =catalog.find(1)->getAttribute<Euclid::SourceCatalog::SpectroscopicRedshift>();
+  auto photo_z2 =catalog.find(2)->getAttribute<Euclid::SourceCatalog::SpectroscopicRedshift>();
 
-  // Redundant definition of the z column
-  BOOST_CHECK_THROW(
-      fconf = cf::SpectroscopicRedshiftCatalogConfiguration(options_map_2_z),
-      Elements::Exception);
+  BOOST_CHECK(Elements::isEqual(0.,photo_z1->getError()));
+  BOOST_CHECK(Elements::isEqual(0.,photo_z2->getError()));
 
-  // Nominal use-cases with error
-  fconf = cf::SpectroscopicRedshiftCatalogConfiguration(options_map_1_deltaZ_1);
-  fconf.getCatalog();
-  fconf = cf::SpectroscopicRedshiftCatalogConfiguration(options_map_1_deltaZ_2);
-  auto catalog_with_err = fconf.getCatalog();
-  //check the error
-  photo_z =catalog_with_err.find(1)->getAttribute<Euclid::SourceCatalog::SpectroscopicRedshift>();
-  BOOST_CHECK(Elements::isEqual(0.01,photo_z->getError()));
-
-  // Redundant definition of the error column
-  BOOST_CHECK_THROW(
-      fconf = cf::SpectroscopicRedshiftCatalogConfiguration(
-          options_map_2_deltaZ), Elements::Exception);
 }
 
-//-----------------------------------------------------------------------------
+BOOST_FIXTURE_TEST_CASE(SpectroscopicRedshiftCatalogConfiguration_constructor_z_plus_index_test, SpectroscopicRedshiftCatalogConfiguration_Fixture) {
+
+  BOOST_TEST_MESSAGE(" ");
+  BOOST_TEST_MESSAGE("--> Testing the constructor, Z + index column");
+  BOOST_TEST_MESSAGE(" ");
+
+  cf::SpectroscopicRedshiftCatalogConfiguration fconf(options_map_min_working_2);
+  auto catalog = fconf.getCatalog();
+  auto photo_z1 =catalog.find(1)->getAttribute<Euclid::SourceCatalog::SpectroscopicRedshift>();
+  auto photo_z2 =catalog.find(2)->getAttribute<Euclid::SourceCatalog::SpectroscopicRedshift>();
+
+  // All error values must be set to zero
+  BOOST_CHECK(Elements::isEqual(0.,photo_z1->getError()));
+  BOOST_CHECK(Elements::isEqual(0.,photo_z2->getError()));
+
+}
+
+BOOST_FIXTURE_TEST_CASE(SpectroscopicRedshiftCatalogConfiguration_constructor_index_test, SpectroscopicRedshiftCatalogConfiguration_Fixture) {
+
+  BOOST_TEST_MESSAGE(" ");
+  BOOST_TEST_MESSAGE("--> Testing the constructor Z plus index");
+  BOOST_TEST_MESSAGE(" ");
+
+  // Z index + Z column are Forbidden
+  BOOST_CHECK_THROW(cf::SpectroscopicRedshiftCatalogConfiguration fconf(options_map_z_index), Elements::Exception);
+}
+
+
+BOOST_FIXTURE_TEST_CASE(SpectroscopicRedshiftCatalogConfiguration_constructor_z_index__plus_indexname_test, SpectroscopicRedshiftCatalogConfiguration_Fixture) {
+
+  BOOST_TEST_MESSAGE(" ");
+  BOOST_TEST_MESSAGE("--> Testing the constructor Z, deltaZ plus index and index name");
+  BOOST_TEST_MESSAGE(" ");
+
+  // deltaZ index + delatZ name are Forbidden
+  BOOST_CHECK_THROW(cf::SpectroscopicRedshiftCatalogConfiguration fconf(options_map_z_deltaZ_index_plus_name), Elements::Exception);
+}
 
 BOOST_AUTO_TEST_SUITE_END ()
