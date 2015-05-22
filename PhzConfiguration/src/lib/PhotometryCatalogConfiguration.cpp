a93efaa4b21825f042127c7bcd9186ea82643c6c
Nikolaos Apostolakos <nikoapos@gmail.com>
2015-05-22 12:44:59 +0000

    Replaced the string with the option names of the PhotometryCatalogConfiguration with global constants. [Redmine Issue #8269]
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/PhosphorosCore/trunk@7707 463500fe-d6be-4184-9b20-47717e75ecac

--- PhzConfiguration/src/lib/PhotometryCatalogConfiguration.cpp
+++ PhzConfiguration/src/lib/PhotometryCatalogConfiguration.cpp
@@ -22,12 +22,15 @@ namespace PhzConfiguration {
 
 static Elements::Logging logger = Elements::Logging::getLogger("PhotometryCatalogConfiguration");
 
+static const std::string FILTER_NAME_MAPPING {"filter-name-mapping"};
+static const std::string MISSING_PHOTOMETRY_FLAG {"missing-photometry-flag"};
+
 po::options_description PhotometryCatalogConfiguration::getProgramOptions() {
   po::options_description options = CatalogConfiguration::getProgramOptions();
   options.add_options()
-        ("filter-name-mapping", po::value<std::vector<std::string>>(),
+        (FILTER_NAME_MAPPING.c_str(), po::value<std::vector<std::string>>(),
             "The mapping of the flux and error columns of a filter FORMAT=\"filter flux_name error_name\"")
-        ("missing-photometry-flag", po::value<double>(),
+        (MISSING_PHOTOMETRY_FLAG.c_str(), po::value<double>(),
             "It is a flag value for missing flux for a source, default value: -99.");
   return options;
 }
@@ -36,26 +39,26 @@ PhotometryCatalogConfiguration::PhotometryCatalogConfiguration(const std::map<st
         : CatalogConfiguration(options) {
   // First create the vector with the filter name mapping based on the user parameters
   std::vector<std::pair<std::string, std::pair<std::string, std::string>>> filter_name_mapping {};
-  auto mapping_iter = options.find("filter-name-mapping");
+  auto mapping_iter = options.find(FILTER_NAME_MAPPING);
   if (mapping_iter != options.end()) {
     for (auto& filter_mapping_option : mapping_iter->second.as<std::vector<std::string>>()) {
       smatch match_res;
       regex expr {"\\s*([^\\s]+)\\s+([^\\s]+)\\s+([^\\s]+)\\s*"};
       if (!regex_match(filter_mapping_option, match_res, expr)) {
-        logger.error() << "Malformed filter-name-mapping: " << filter_mapping_option;
-        throw Elements::Exception() << "Malformed filter-name-mapping: " << filter_mapping_option;
+        logger.error() << "Malformed " << FILTER_NAME_MAPPING << ": " << filter_mapping_option;
+        throw Elements::Exception() << "Malformed " << FILTER_NAME_MAPPING << ": " << filter_mapping_option;
       }
       filter_name_mapping.emplace_back(match_res.str(1), std::make_pair(match_res.str(2), match_res.str(3)));
     }
   }
   if (filter_name_mapping.size() < 2) {
-    logger.error() << "Found only " << filter_name_mapping.size() << " filter-name-mapping parameters";
-    throw Elements::Exception() << "Need two or more source photometries to operate (check filter-name-mapping option)";
+    logger.error() << "Found only " << filter_name_mapping.size() << " " << FILTER_NAME_MAPPING << " parameters";
+    throw Elements::Exception() << "Need two or more source photometries to operate (check " << FILTER_NAME_MAPPING << " option)";
   }
   
   // Get the flag
   double missing_photo_flag {-99.};
-  auto flag_iter = options.find("missing-photometry-flag");
+  auto flag_iter = options.find(MISSING_PHOTOMETRY_FLAG);
   if (flag_iter != options.end()) {
      missing_photo_flag = flag_iter->second.as<double>();
   }
