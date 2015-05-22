c80f0843d6fd828ab091f4c88a95cf0e8a62a472
Nikolaos Apostolakos <nikoapos@gmail.com>
2015-05-22 10:17:11 +0000

    Replaced the string "igm-absorption-type" with a global variable [Redmine Issue #8269]
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/PhosphorosCore/trunk@7702 463500fe-d6be-4184-9b20-47717e75ecac

--- PhzConfiguration/src/lib/IgmConfiguration.cpp
+++ PhzConfiguration/src/lib/IgmConfiguration.cpp
@@ -15,18 +15,20 @@ namespace po = boost::program_options;
 namespace Euclid {
 namespace PhzConfiguration {
 
+static const std::string IGM_ABSORPTION_TYPE {"igm-absorption-type"};
+
 po::options_description IgmConfiguration::getProgramOptions() {
   po::options_description options {"IGM absorption options"};
   options.add_options()
-  ("igm-absorption-type", po::value<std::string>(),
+  (IGM_ABSORPTION_TYPE.c_str(), po::value<std::string>(),
         "The type of IGM absorption to apply (one of OFF, MADAU, MEIKSIN, INOUE)");
   return options;
 }
 
 PhzModeling::PhotometryGridCreator::IgmAbsorptionFunction IgmConfiguration::getIgmAbsorptionFunction() {
   auto type = getIgmAbsorptionType();
-  if (m_options["igm-absorption-type"].empty()) { 
-    throw Elements::Exception() << "Missing mandatory parameter igm-absorption-type";
+  if (m_options[IGM_ABSORPTION_TYPE].empty()) { 
+    throw Elements::Exception() << "Missing mandatory parameter " << IGM_ABSORPTION_TYPE;
   }
   if (type == "OFF") {
     return PhzModeling::NoIgmFunctor{};
@@ -40,14 +42,14 @@ PhzModeling::PhotometryGridCreator::IgmAbsorptionFunction IgmConfiguration::getI
   if (type == "INOUE") {
     return PhzModeling::InoueIgmFunctor{};
   }
-  throw Elements::Exception() << "Unknown IGM absorption type \"" << type << "\"";
+  throw Elements::Exception() << "Unknown " << IGM_ABSORPTION_TYPE << " option \"" << type << "\"";
 }
 
 std::string IgmConfiguration::getIgmAbsorptionType() {
-  if (!m_options["igm-absorption-type"].empty()) { 
-    return m_options["igm-absorption-type"].as<std::string>();
+  if (!m_options[IGM_ABSORPTION_TYPE].empty()) { 
+    return m_options[IGM_ABSORPTION_TYPE].as<std::string>();
   }
-  throw Elements::Exception() << "Missing mandatory parameter igm-absorption-type";
+  throw Elements::Exception() << "Missing mandatory parameter " << IGM_ABSORPTION_TYPE;
 }
 
 }
