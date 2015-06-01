94f6a9e3a9d97dd7e4d12cf6345d5d63fa4c77f8
Nicolas Morisset <Nicolas.Morisset@unige.ch>
2015-06-01 10:07:12 +0000

    Modified the serialization of the PhotometryGridInfo class to serialize vector of strings instead of a vector of QualifiedNames because the update of the latest version of clang was breaking the compilation*
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/PhosphorosCore/trunk@7769 463500fe-d6be-4184-9b20-47717e75ecac

--- PhzDataModel/PhzDataModel/serialization/PhotometryGridInfo.h
+++ PhzDataModel/PhzDataModel/serialization/PhotometryGridInfo.h
@@ -11,15 +11,36 @@
 #include <boost/serialization/string.hpp>
 #include "GridContainer/serialization/GridContainer.h"
 #include "PhzDataModel/PhotometryGridInfo.h"
+#include <boost/serialization/split_free.hpp>
 
 namespace boost {
 namespace serialization {
 
 template<typename Archive>
-void serialize(Archive& ar, Euclid::PhzDataModel::PhotometryGridInfo& t, const unsigned int) {
-  ar & t.axes;
-  ar & t.igm_method;
-  ar & t.filter_names;
+void save(Archive& ar, const Euclid::PhzDataModel::PhotometryGridInfo& t, const unsigned int) {
+  ar << t.axes;
+  ar << t.igm_method;
+  std::vector<std::string> names_as_strings {};
+  for (auto& name : t.filter_names) {
+    names_as_strings.push_back(name.qualifiedName());
+  }
+  ar << names_as_strings;
+}
+
+template<typename Archive>
+void load(Archive& ar, Euclid::PhzDataModel::PhotometryGridInfo& t, const unsigned int) {
+  ar >> t.axes;
+  ar >> t.igm_method;
+  std::vector<std::string> names_as_strings;
+  ar >> names_as_strings;
+  for (auto& name : names_as_strings) {
+    t.filter_names.push_back(name);
+  }
+}
+
+template<typename Archive>
+void serialize(Archive& ar, Euclid::PhzDataModel::PhotometryGridInfo& t, const unsigned int version) {
+  split_free(ar, t, version);
 }
 
 } // end of namespace serialization
