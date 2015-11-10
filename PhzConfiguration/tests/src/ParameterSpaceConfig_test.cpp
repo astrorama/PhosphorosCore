4f1cc64c65cc0734296aa8f689622d7b49cfab04
Florian Dubath <Florian.Dubath@unige.ch>
2015-11-10 14:14:02 +0000

    Test file [Redmine Issue ##9783]
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/PhosphorosCore/branches/LuminosityPhosphorosCore@10015 463500fe-d6be-4184-9b20-47717e75ecac

---
+++ PhzConfiguration/tests/src/ParameterSpaceConfig_test.cpp
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2012-2020 Euclid Science Ground Segment
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3.0 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/**
+ * @file tests/src/ParameterSpaceConfig_test.cpp
+ * @date 2015/11/06
+ * @author Florian Dubath
+ */
+
+#include <boost/test/unit_test.hpp>
+#include <vector>
+#include "PhzConfiguration/ParameterSpaceConfig.h"
+#include "ConfigManager_fixture.h"
+#include "XYDataset/QualifiedName.h"
+
+using namespace Euclid::PhzConfiguration;
+namespace po = boost::program_options;
+namespace fs = boost::filesystem;
+
+//-----------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_SUITE (AuxDataDirConfig_test)
+
+
+
+
+BOOST_FIXTURE_TEST_CASE( NotInitializedGetter_test, ConfigManager_fixture ) {
+  // Given
+  config_manager.registerConfiguration<ParameterSpaceConfig>();
+  config_manager.closeRegistration();
+
+  // Then
+  BOOST_CHECK_THROW(config_manager.getConfiguration<ParameterSpaceConfig>().getParameterSpaceRegions(), Elements::Exception);
+}
+
+
+//-----------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_SUITE_END ()
+
+
