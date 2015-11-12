a73525f9b59c8638c8a784308a6bc8cf7b292bc2
Florian Dubath <Florian.Dubath@unige.ch>
2015-11-12 15:03:22 +0000

    Writing the missing comments [Redmine Issue ##9775]
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/PhosphorosCore/branches/LuminosityPhosphorosCore@10067 463500fe-d6be-4184-9b20-47717e75ecac

--- PhzConfiguration/PhzConfiguration/RedshiftConfig.h
+++ PhzConfiguration/PhzConfiguration/RedshiftConfig.h
@@ -40,56 +40,61 @@ namespace PhzConfiguration {
 
 /**
  * @class RedshiftConfig
- * @brief
- * This class defines the Redshift parameter options
- * @details
- * The parameters available are:
- * - \b z-range : double type, min max step e.g. "0. 2. 0.5"
- *          - min as minimum redshift value
- *          - max as maximum redshift value and max > min
- *          - step, step to be used for the interval
- *  Note that the space character is the separator.
- *  You can add several z-range parameters but they should not overlap. The
- *  following example is not allowed:
- *  - z-range = 0. 2. 0.5
- *  - z-range = 1.5 3. 1.
- *  as 1.5 is less than 2 of the first range. \n
- * A regex is applied for z-range: `(((\\d+(\\.\\d*)?)|(\\.\\d+))($|\\s+)){3}`
  *
- * - \b z-value : double, redshift value e.g. 4. \n
- * You can add as many z-value parameters as you want but each z-value
- * parameter must contain only one value.\n
- * A regex is applied for z-value : `((\\d+(\\.\\d*)?)|(\\.\\d+))($|\\s+)`
- * @throw Element::Exception
- * - Invalid range(s) for the z-range option!
- * - Invalid character(s) for the z-range option
- * - Invalid character(s) for the z-value option
+ * @brief This class defines the Redshift parameter options.
  */
 class RedshiftConfig : public Configuration::Configuration {
 
 public:
-
-  RedshiftConfig(long manager_id);
 
   /**
    * @brief Destructor
    */
   virtual ~RedshiftConfig() = default;
 
+  /**
+   * @brief constructor
+   */
   std::map<std::string, OptionDescriptionList> getProgramOptions() override;
 
+  /**
+    * @details
+    * This class define the "z-range" and "z-value" options into the
+    * "Redshift options" group
+    */
+   RedshiftConfig(long manager_id);
+
+  /**
+   * @details
+   * Compute the redshift axis values.
+   * The parameters available are:
+   * - \b z-range : double type, min max step e.g. "0. 2. 0.5"
+   *          - min as minimum redshift value
+   *          - max as maximum redshift value and max > min
+   *          - step, step to be used for the interval
+   *  Note that the space character is the separator.
+   *  You can add several z-range parameters but they should not overlap. The
+   *  following example is not allowed:
+   *  - z-range = 0. 2. 0.5
+   *  - z-range = 1.5 3. 1.
+   *  as 1.5 is less than 2 of the first range. \n
+   * A regex is applied for z-range: `(((\\d+(\\.\\d*)?)|(\\.\\d+))($|\\s+)){3}`
+   *
+   * - \b z-value : double, redshift value e.g. 4. \n
+   * You can add as many z-value parameters as you want but each z-value
+   * parameter must contain only one value.\n
+   * A regex is applied for z-value : `((\\d+(\\.\\d*)?)|(\\.\\d+))($|\\s+)`
+   * @throw Element::Exception
+   * - Invalid range(s) for the z-range option!
+   * - Invalid character(s) for the z-range option
+   * - Invalid character(s) for the z-value option
+   * */
   void initialize(const UserValues& args) override;
 
   /**
    * @brief
    * This function provides a redshift list
-   * @details
-   * @throw Element::Exception
-   * - Missing  <filter-root-path> or unknown filter dataset provider options
-   * - Invalid range(s) for the z-range option!
-   * - Invalid character(s) for the z-range option
-   * - Invalid character(s) for the z-value option
-   * - Empty filter list
+   *
    * @return
    * A vector of QualifiedName type
    */
