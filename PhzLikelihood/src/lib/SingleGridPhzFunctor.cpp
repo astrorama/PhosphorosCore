d2ace844646a62eb7063a041951aa526607c6429
Nikolaos Apostolakos <nikoapos@gmail.com>
2016-02-07 20:43:21 +0000

    Modified the SingleGridPhzFunctor to scale the posterior so the peak is 1 and to report the best chi square based on this peak [Redmine Issue #10225]
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/PhosphorosCore/branches/Task10225@11128 463500fe-d6be-4184-9b20-47717e75ecac

--- PhzLikelihood/src/lib/SingleGridPhzFunctor.cpp
+++ PhzLikelihood/src/lib/SingleGridPhzFunctor.cpp
@@ -29,7 +29,7 @@ void SingleGridPhzFunctor::operator()(const SourceCatalog::Photometry& source_ph
   auto likelihood_res = m_likelihood_func(source_phot, m_phot_grid);
   PhzDataModel::LikelihoodGrid likelihood_grid {std::move(std::get<0>(likelihood_res))};
   PhzDataModel::ScaleFactordGrid scale_factor_grid {std::move(std::get<1>(likelihood_res))};
-  double best_chi_square = -2. * std::get<2>(likelihood_res);
+  double likelihood_best_chi_square = -2. * std::get<2>(likelihood_res);
   
   // copy the likelihood Grid
   PhzDataModel::LikelihoodGrid posterior_grid{likelihood_grid.getAxesTuple()};
@@ -48,7 +48,14 @@ void SingleGridPhzFunctor::operator()(const SourceCatalog::Photometry& source_ph
   // Get an iterator to the scale factor of the best fit model
   auto scale_factor_result = scale_factor_grid.begin();
   scale_factor_result.fixAllAxes(best_fit);
-
+  
+  // Scale the posterior to have peak at 1 and compute its best chi square
+  double posterior_peak = *best_fit;
+  for (auto& v : posterior_grid) {
+    v = v / posterior_peak;
+  }
+  double posterior_best_chi_square = likelihood_best_chi_square - 2. * std::log(posterior_peak);
+  
   // Calculate the 1D PDF
   auto pdf_1D = m_marginalization_func(posterior_grid);
   
@@ -65,7 +72,7 @@ void SingleGridPhzFunctor::operator()(const SourceCatalog::Photometry& source_ph
   results.getResult<PhzDataModel::SourceResultType::REGION_BEST_MODEL_SCALE_FACTOR>().emplace(
                 std::make_pair(m_region_name, *scale_factor_result));
   results.getResult<PhzDataModel::SourceResultType::REGION_BEST_MODEL_CHI_SQUARE>().emplace(
-                std::make_pair(m_region_name, best_chi_square));
+                std::make_pair(m_region_name, posterior_best_chi_square));
 }
   
 } // end of namespace PhzLikelihood
