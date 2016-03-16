d59791b725f03033074d8d9a5e01b7a68dec95e6
Nikolaos Apostolakos <nikoapos@gmail.com>
2016-03-16 11:06:40 +0000

    Added a helper method for filling the default options during testing
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/PhosphorosCore/trunk@12374 463500fe-d6be-4184-9b20-47717e75ecac

--- PhzConfiguration/tests/src/ConfigManager_fixture.h
+++ PhzConfiguration/tests/src/ConfigManager_fixture.h
@@ -17,6 +17,19 @@ struct ConfigManager_fixture {
   
   Euclid::Configuration::ConfigManager& config_manager = 
                       Euclid::Configuration::ConfigManager::getInstance(timestamp);
+  
+  template <typename T>
+  std::map<std::string, boost::program_options::variable_value> registerConfigAndGetDefaultOptionsMap() {
+    config_manager.registerConfiguration<T>();
+    auto options = config_manager.closeRegistration();
+    
+    boost::program_options::variables_map map {};
+    auto parsed = boost::program_options::command_line_parser({NULL}, 0)
+                        .options(options).run();
+    boost::program_options::store(parsed, map);
+    
+    return map;
+  }
  
 };
 
