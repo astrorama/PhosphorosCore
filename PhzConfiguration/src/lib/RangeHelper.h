fdadee57508e67f4b667d38960da8a11d10f32a3
Nikolaos Apostolakos <nikoapos@gmail.com>
2016-04-13 10:47:26 +0000

    Fixed the behavior of ranges in random order [Redmine Issue #10568] Fixed the behavior of ranges with invalid step [Redmine Issue #10571] Fixed the double arithmetic issue resulting with the max of ranges being duplicated [Redmine Issue #10573]
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/PhosphorosCore/branches/0.5-RC@13023 463500fe-d6be-4184-9b20-47717e75ecac

---
+++ PhzConfiguration/src/lib/RangeHelper.h
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2012-2020 Euclid Science Ground Segment    
+ *  
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free 
+ * Software Foundation; either version 3.0 of the License, or (at your option)  
+ * any later version.  
+ *  
+ * This library is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more  
+ * details.  
+ *  
+ * You should have received a copy of the GNU Lesser General Public License 
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA  
+ */
+
+/* 
+ * @file RangeHelper.h
+ * @author nikoapos
+ */
+
+#ifndef PHZCONFIGURATION_RANGEHELPER_H
+#define PHZCONFIGURATION_RANGEHELPER_H
+
+#include <vector>
+#include <tuple>
+#include <string>
+#include <sstream>
+#include <regex>
+
+namespace Euclid {
+namespace PhzConfiguration {
+
+/**
+ * @brief Method for parsing a set of strings representing ranges
+ * @details
+ * The ranges are of the format "min max step". The result is a vector of tuples
+ * which contain the min, max and step values. The result is ordered based on the
+ * values of the ranges and not the order of the input. All the exceptions thrown
+ * contain the text of the invalid range as their message, together with the
+ * reason of the failure.
+ * @param ranges_list
+ *    The ranges to parse
+ * @return 
+ *    The parsed ranges
+ * @throws Elements::Exception
+ *    If a range does not follow the format "min max step"
+ * @throws Elements::Exception
+ *    If a range has min > max
+ * @throws Elements::Exception
+ *    If a range has non positive step
+ */
+static std::vector<std::tuple<double, double, double>> parseRanges(const std::vector<std::string>& ranges_list) {
+  
+  std::vector<std::tuple<double, double, double>> result {};
+  for (auto& range_string : ranges_list) {
+    
+    // Check that the format of the range is correct, using a regular expression
+    if (!std::regex_match(range_string, std::regex{"((-?(\\d+(\\.\\d*)?)|(-?\\.\\d+))($|\\s+)){3}"})) {
+      throw Elements::Exception() << range_string << " (wrong format)";
+    }
+    
+    double min {};
+    double max {};
+    double step {};
+    std::stringstream range_stream {range_string};
+    range_stream >> min >> max >> step;
+    
+    if (min > max) {
+      throw Elements::Exception() << range_string << " (invalid limits)";
+    }
+
+    if (step <= 0) {
+      throw Elements::Exception() << range_string << " (invalid step)";
+    }
+    
+    result.emplace_back(min, max, step);
+  }
+  
+  std::sort(result.begin(), result.end());
+  return result;
+}
+
+} // PhzConfiguration namespace
+} // Euclid namespace
+
+#endif /* PHZCONFIGURATION_RANGEHELPER_H */
+
--- PhzConfiguration/tests/src/RedshiftConfig_test.cpp
+++ PhzConfiguration/tests/src/RedshiftConfig_test.cpp
@@ -150,10 +150,100 @@ BOOST_FIXTURE_TEST_CASE(getZList_more_ranges_function_test, ConfigManager_fixtur
 }
 
 //-----------------------------------------------------------------------------
-// Test the getZList function and forbidden ranges
+// Test the getZList function and add several ranges in random order
 //-----------------------------------------------------------------------------
 
-BOOST_FIXTURE_TEST_CASE(getZList_forbidden_ranges_function_test, ConfigManager_fixture) {
+BOOST_FIXTURE_TEST_CASE(getZList_more_ranges_random_order_function_test, ConfigManager_fixture) {
+  // Given
+  config_manager.registerConfiguration<RedshiftConfig>();
+  config_manager.closeRegistration();
+  std::map<std::string, po::variable_value> options_map { };
+
+  std::vector<std::string> z_ranges_vector;
+
+  z_ranges_vector.push_back("3. 6. 1.");
+  z_ranges_vector.push_back("0. 2 0.5");
+
+
+  options_map["z-range"].value() = boost::any(z_ranges_vector);
+
+  // When
+  config_manager.initialize(options_map);
+  auto z_list = config_manager.getConfiguration<RedshiftConfig>().getZList().at("");
+
+  // Then
+  BOOST_CHECK_EQUAL(z_list[1], 0.5);
+  BOOST_CHECK_EQUAL(z_list[3], 1.5);
+  BOOST_CHECK_EQUAL(z_list[5], 3);
+  BOOST_CHECK_EQUAL(z_list[8], 6);
+
+}
+
+//-----------------------------------------------------------------------------
+// Test the getZList function and invalid range limits
+//-----------------------------------------------------------------------------
+
+BOOST_FIXTURE_TEST_CASE(getZList_invalid_range_limits_test, ConfigManager_fixture) {
+  // Given
+  config_manager.registerConfiguration<RedshiftConfig>();
+  config_manager.closeRegistration();
+  std::map<std::string, po::variable_value> options_map { };
+
+  std::vector<std::string> z_ranges_vector;
+
+  z_ranges_vector.push_back("1. .5 0.5");
+
+  options_map["z-range"].value() = boost::any(z_ranges_vector);
+
+  // Then
+  BOOST_CHECK_THROW(config_manager.initialize(options_map), Elements::Exception);
+}
+
+//-----------------------------------------------------------------------------
+// Test the getZList function and negative range step
+//-----------------------------------------------------------------------------
+
+BOOST_FIXTURE_TEST_CASE(getZList_negative_range_step_test, ConfigManager_fixture) {
+  // Given
+  config_manager.registerConfiguration<RedshiftConfig>();
+  config_manager.closeRegistration();
+  std::map<std::string, po::variable_value> options_map { };
+
+  std::vector<std::string> z_ranges_vector;
+
+  z_ranges_vector.push_back("0. 1. -0.5");
+
+  options_map["z-range"].value() = boost::any(z_ranges_vector);
+
+  // Then
+  BOOST_CHECK_THROW(config_manager.initialize(options_map), Elements::Exception);
+}
+
+//-----------------------------------------------------------------------------
+// Test the getZList function and zero range step
+//-----------------------------------------------------------------------------
+
+BOOST_FIXTURE_TEST_CASE(getZList_zero_range_step_test, ConfigManager_fixture) {
+  // Given
+  config_manager.registerConfiguration<RedshiftConfig>();
+  config_manager.closeRegistration();
+  std::map<std::string, po::variable_value> options_map { };
+
+  std::vector<std::string> z_ranges_vector;
+
+  z_ranges_vector.push_back("0. 1. 0");
+
+  options_map["z-range"].value() = boost::any(z_ranges_vector);
+
+  // Then
+  BOOST_CHECK_THROW(config_manager.initialize(options_map), Elements::Exception);
+}
+
+//-----------------------------------------------------------------------------
+// Test the getZList function and overlapping ranges
+//-----------------------------------------------------------------------------
+
+BOOST_FIXTURE_TEST_CASE(getZList_overlapping_ranges_function_test, ConfigManager_fixture) {
   // Given
   config_manager.registerConfiguration<RedshiftConfig>();
   config_manager.closeRegistration();
