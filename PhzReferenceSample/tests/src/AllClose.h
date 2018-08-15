218ce16c297a19168cd7ea1f95cbecacffd98ded
Alejandro Álvarez Ayllón <a.alvarezayllon@gmail.com>
2018-08-15 10:00:45 +0200

    Use Elements::isNotEqual for ReferenceSample tests

--- PhzReferenceSample/tests/src/AllClose.h
+++ PhzReferenceSample/tests/src/AllClose.h
@@ -1,17 +1,20 @@
+#include <ElementsKernel/Real.h>
+#include <iomanip>
+
 namespace Euclid {
 namespace XYDataset {
 
+
 boost::test_tools::predicate_result checkAllClose(const XYDataset& a, const XYDataset &b) {
   boost::test_tools::predicate_result res(true);
-  boost::test_tools::close_at_tolerance<double> is_close(boost::test_tools::percent_tolerance(1e-5));
 
   if (a.size() != b.size()) {
     res = false;
     res.message() << "Different sizes";
   }
   else {
-    for (auto i = a.begin(), j = b.begin(); i != a.end(); ++i, ++j) {
-      if (!is_close(i->first, j->first) || !is_close(i->second, j->second)) {
+    for (auto i = a.begin(), j = b.begin(); i != a.end() && j != b.end(); ++i, ++j) {
+      if(Elements::isNotEqual(i->first, j->first) || Elements::isNotEqual(i->second, j->second)) {
         res = false;
         res.message()
           << '<' << i->first << ',' << i->second << '>'
