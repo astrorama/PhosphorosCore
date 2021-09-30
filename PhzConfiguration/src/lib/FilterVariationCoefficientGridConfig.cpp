6007b558a2e8d9a7b8f0505ac832ec9cdecf45a8
Florian Dubath <florian.dubath@unige.ch>
2021-09-30 11:16:42 +0200

    remove duplicated code and add tests

--- PhzConfiguration/src/lib/FilterVariationCoefficientGridConfig.cpp
+++ PhzConfiguration/src/lib/FilterVariationCoefficientGridConfig.cpp
@@ -107,52 +107,6 @@ void FilterVariationCoefficientGridConfig::initialize(const UserValues& args) {
       default:
         throw Elements::Exception() << "Unknown model grid format";
     }
-
-    // Here we try to get the PhotometricBandMappingConfig. If this is throws, it
-    // means the PhotometryGridConfig is not used together with a Photometry catalog,
-    // so we need to do nothing. Otherwise we set the photometries to the correct
-    // filters
-    std::shared_ptr<std::vector<std::string>> filter_names {nullptr};
-    try {
-      auto& filter_mapping = getDependency<Euclid::Configuration::PhotometricBandMappingConfig>().getPhotometricBandMapping();
-      filter_names = std::make_shared<std::vector<std::string>>();
-      for (auto& pair : filter_mapping) {
-        filter_names->push_back(pair.first);
-      }
-    } catch (const Elements::Exception& e) {
-      // The exception means the PhotometricBandMappingConfig was not registered
-    }
-
-    if (filter_names != nullptr) {
-      // Check if we need to change the photometries
-      bool same = filter_names->size() == m_info.filter_names.size();
-      if (same) {
-        same = std::equal(m_info.filter_names.begin(), m_info.filter_names.end(), filter_names->begin());
-      }
-
-      if (!same) {
-        // Check that we have all the catalog photometries in the grid
-        for (auto& f : *filter_names) {
-          if (std::count(m_info.filter_names.begin(), m_info.filter_names.end(), f) == 0) {
-            throw Elements::Exception() << "Filter " << f << " missing from the model grid";
-          }
-        }
-
-        // Here we know we need to make new photometries and replace the members
-        m_info.filter_names.clear();
-        m_info.filter_names.insert(m_info.filter_names.begin(), filter_names->begin(), filter_names->end());
-
-        for (auto& pair : m_grids) {
-          for (auto& p : pair.second) {
-            std::vector<SourceCatalog::FluxErrorPair> values {};
-            for (auto& f : *filter_names) {
-              values.emplace_back(*(p.find(f)));
-            }
-            p = SourceCatalog::Photometry(filter_names, std::move(values));
-          }
-        }
-      }
-    }
   }
 }
 
