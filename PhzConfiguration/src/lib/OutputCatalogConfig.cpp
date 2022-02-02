4c305356d70c4d1f034583f6f7c6d23ce99a62ce
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2022-02-02 10:47:15 +0100

    Honor length of string ids

--- PhzConfiguration/src/lib/OutputCatalogConfig.cpp
+++ PhzConfiguration/src/lib/OutputCatalogConfig.cpp
@@ -1,20 +1,18 @@
-/*
- * Copyright (C) 2012-2020 Euclid Science Ground Segment
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 3.0 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- */
+// Copyright (C) 2012-2022 Euclid Science Ground Segment
+//
+// This library is free software; you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation; either version 3.0 of the License, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 /**
  * @file src/lib/OutputCatalogConfig.cpp
@@ -78,7 +76,7 @@ void OutputCatalogConfig::initialize(const UserValues& args) {
     throw Elements::Exception() << "Could not find the ID column on the input catalog";
   }
   auto id_info = column_info->getDescription(*id_index);
-  m_column_handler_list.emplace_back(new PhzOutput::ColumnHandlers::Id {id_info.type});
+  m_column_handler_list.emplace_back(new PhzOutput::ColumnHandlers::Id {id_info.type, id_info.size});
 
   if (args.at(OUTPUT_CATALOG_FORMAT).as<std::string>() == "ASCII") {
     m_format = PhzOutput::PhzCatalog::Format::ASCII;
