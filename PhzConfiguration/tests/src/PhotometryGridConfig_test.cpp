34c340b76014a3662e8ff2e306b703155f19b722
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2022-08-22 15:44:42 +0200

    Add test for exclude-filter

--- PhzConfiguration/tests/src/PhotometryGridConfig_test.cpp
+++ PhzConfiguration/tests/src/PhotometryGridConfig_test.cpp
@@ -27,17 +27,17 @@
 #include <boost/test/unit_test.hpp>
 #include <fstream>
 
+#include "ConfigManager_fixture.h"
+#include "Configuration/PhotometricBandMappingConfig.h"
 #include "ElementsKernel/Temporary.h"
-
 #include "GridContainer/serialize.h"
+#include "PhzConfiguration/PhotometryGridConfig.h"
 #include "PhzDataModel/PhotometryGrid.h"
 #include "PhzDataModel/serialization/PhotometryGridInfo.h"
 
-#include "ConfigManager_fixture.h"
-#include "PhzConfiguration/PhotometryGridConfig.h"
-
 using namespace Euclid::PhzConfiguration;
 using namespace Euclid::PhzDataModel;
+using namespace Euclid::Configuration;
 namespace po = boost::program_options;
 namespace fs = boost::filesystem;
 
@@ -49,8 +49,9 @@ struct PhotometryGridConfig_fixture : public ConfigManager_fixture {
   fs::path          intermediate_dir = temp_dir.path() / "Intermediate";
   std::string       catalog_type{"CatalogType"};
   std::string       filename{"model_grid.dat"};
-  fs::path          relative = fs::path{"relative"} / filename;
-  fs::path          absolute = temp_dir.path() / "absolute" / filename;
+  fs::path          relative       = fs::path{"relative"} / filename;
+  fs::path          absolute       = temp_dir.path() / "absolute" / filename;
+  fs::path          filter_mapping = temp_dir.path() / "filter-mapping.txt";
 
   std::map<std::string, po::variable_value> options_map{};
 
@@ -61,11 +62,10 @@ struct PhotometryGridConfig_fixture : public ConfigManager_fixture {
   std::map<std::string, PhotometryGrid>         grid_map{};
 
   PhotometryGridConfig_fixture() {
-
     grid_map.insert(std::pair<const std::string, PhotometryGrid>(
         "", PhotometryGrid{createAxesTuple(zs, ebvs, reddening_curves, seds),
-                           std::vector<std::string>{"Filter1", "Filter2"}}));
-    PhotometryGridInfo info{grid_map, "OFF", {"Filter1"}, {}};
+                           std::vector<std::string>{"Filter1", "Filter2", "Filter3"}}));
+    PhotometryGridInfo info{grid_map, "OFF", {"Filter1"}, {{"Filter1"}, {"Filter2"}, {"Filter3"}}};
     {
       fs::create_directories(intermediate_dir / catalog_type / "ModelGrids");
       std::ofstream                   stream{(intermediate_dir / catalog_type / "ModelGrids" / filename).string()};
@@ -86,6 +86,12 @@ struct PhotometryGridConfig_fixture : public ConfigManager_fixture {
       boost::archive::binary_oarchive boa{stream};
       boa << info;
       Euclid::GridContainer::gridBinaryExport(stream, grid_map.at(""));
+    }
+    {
+      std::ofstream stream{filter_mapping.string()};
+      stream << "Filter1 FLUX_FILTER1 FLUXERR_FILTER1 0 3 NONE\n";
+      stream << "Filter2 FLUX_FILTER1 FLUXERR_FILTER1 0 3 NONE\n";
+      stream << "Filter3 FLUX_FILTER3 FLUXERR_FILTER3 0 3 NONE\n";
     }
 
     options_map["intermediate-products-dir"].value() = boost::any(intermediate_dir.string());
@@ -192,6 +198,53 @@ BOOST_FIXTURE_TEST_CASE(missingFile_test, PhotometryGridConfig_fixture) {
   options_map[MODEL_GRID_FILE].value() = boost::any(std::string("missing"));
 
   // Then
+  BOOST_CHECK_THROW(config_manager.initialize(options_map), Elements::Exception);
+}
+
+//-----------------------------------------------------------------------------
+
+BOOST_FIXTURE_TEST_CASE(withFilterMapping_test, PhotometryGridConfig_fixture) {
+  config_manager.registerConfiguration<PhotometryGridConfig>();
+  config_manager.registerConfiguration<PhotometricBandMappingConfig>();
+  config_manager.closeRegistration();
+
+  options_map["filter-mapping-file"].value() = boost::any(filter_mapping.string());
+  options_map["exclude-filter"].value()      = boost::any(std::vector<std::string>{});
+  config_manager.initialize(options_map);
+
+  auto& result_grid  = config_manager.getConfiguration<PhotometryGridConfig>().getPhotometryGrid().at("");
+  auto& used_filters = result_grid.getCellManager().filterNames();
+  std::vector<std::string> expected{"Filter1", "Filter2", "Filter3"};
+  BOOST_CHECK_EQUAL_COLLECTIONS(expected.begin(), expected.end(), used_filters.begin(), used_filters.end());
+}
+
+//-----------------------------------------------------------------------------
+
+BOOST_FIXTURE_TEST_CASE(excludeFilters_test, PhotometryGridConfig_fixture) {
+  config_manager.registerConfiguration<PhotometryGridConfig>();
+  config_manager.registerConfiguration<PhotometricBandMappingConfig>();
+  config_manager.closeRegistration();
+
+  options_map["filter-mapping-file"].value() = boost::any(filter_mapping.string());
+  options_map["exclude-filter"].value()      = boost::any(std::vector<std::string>{"Filter2"});
+  config_manager.initialize(options_map);
+
+  auto& result_grid  = config_manager.getConfiguration<PhotometryGridConfig>().getPhotometryGrid().at("");
+  auto& used_filters = result_grid.getCellManager().filterNames();
+  std::vector<std::string> expected{"Filter1", "Filter3"};
+  BOOST_CHECK_EQUAL_COLLECTIONS(expected.begin(), expected.end(), used_filters.begin(), used_filters.end());
+}
+
+//-----------------------------------------------------------------------------
+
+BOOST_FIXTURE_TEST_CASE(misalignedExcludeFilter_test, PhotometryGridConfig_fixture) {
+  config_manager.registerConfiguration<PhotometryGridConfig>();
+  config_manager.registerConfiguration<PhotometricBandMappingConfig>();
+  config_manager.closeRegistration();
+
+  options_map["filter-mapping-file"].value() = boost::any(filter_mapping.string());
+  options_map["exclude-filter"].value()      = boost::any(std::vector<std::string>{"NotAFilterIKnow"});
+
   BOOST_CHECK_THROW(config_manager.initialize(options_map), Elements::Exception);
 }
 
