f26ad37c0962116cc62e609e32609470fce313de
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2022-10-06 22:21:09 +0200

    Make QualifiedNameGroupManager moveable

--- PhzDataModel/PhzDataModel/QualifiedNameGroupManager.h
+++ PhzDataModel/PhzDataModel/QualifiedNameGroupManager.h
@@ -1,4 +1,22 @@
 /**
+ * Copyright (C) 2022 Euclid Science Ground Segment
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3.0 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/**
  * @file PhzDataModel/QualifiedNameGroupManager.h
  * @date 11/02/15
  * @author Nikolaos Apostolakos
@@ -36,7 +54,10 @@ public:
    * @throws Elements::Exception
    *    If any of the given groups overlap
    */
-  QualifiedNameGroupManager(group_list_type groups);
+  explicit QualifiedNameGroupManager(group_list_type groups);
+
+  QualifiedNameGroupManager(const QualifiedNameGroupManager&) = default;
+  QualifiedNameGroupManager(QualifiedNameGroupManager&&)      = default;
 
   /**
    * @brief Destructor
