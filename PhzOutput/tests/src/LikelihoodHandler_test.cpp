95e71a4639a83f191590ee62a2b2bbbc58386e60
Florian Dubath <florian.dubath@unige.ch>
2023-02-21 14:52:04 +0100

    revert to the previous behavio for long OBJECT_ID type

--- PhzOutput/tests/src/LikelihoodHandler_test.cpp
+++ PhzOutput/tests/src/LikelihoodHandler_test.cpp
@@ -59,12 +59,27 @@ class MockSampler {
 struct LikelihoodFixture {
   Elements::TempDir output_path;
   
+
+  std::vector<double>                   zs{0.0, 1.};
+  std::vector<double>                   ebvs{0.0, 1.};
+  std::vector<Euclid::XYDataset::QualifiedName> reddeing_curves{{"red_curve1"}, {"red_curve2"}, {"red_curve3"}};
+  std::vector<Euclid::XYDataset::QualifiedName> seds{{"sed1"}, {"sed2"}, {"sed3"}};
+  Euclid::PhzDataModel::ModelAxesTuple          axes = Euclid::PhzDataModel::createAxesTuple(zs, ebvs, reddeing_curves, seds);
+  
+  Euclid::PhzDataModel::RegionResults results{}; 
+  
   
   Source source_1{"ID_1",{}};
   Source source_2{"ID_002",{}};
   Source source_3{"ID_003",{}};
   Source source_4{"ID_004",{}};
   Source source_5{"ID_005",{}};
+  
+  
+  std::string i_name = output_path.path().generic_string() +"/Index_File_posterior.fits";
+  std::string f_1_name = output_path.path().generic_string() +"/Sample_File_posterior_1.fits";
+  std::string f_2_name = output_path.path().generic_string() +"/Sample_File_posterior_2.fits";
+  std::string f_3_name = output_path.path().generic_string() +"/Sample_File_posterior_3.fits";
 };
 
 //-----------------------------------------------------------------------------
@@ -74,8 +89,6 @@ BOOST_AUTO_TEST_SUITE(Likelihood_test)
 //-----------------------------------------------------------------------------
 
 BOOST_FIXTURE_TEST_CASE(test_PosteriorGridZ, LikelihoodFixture) {
-  std::string i_name = output_path.path().generic_string() +"/Index_File_posterior.fits";
-  std::string f_1_name = output_path.path().generic_string() +"/Sample_File_posterior_1.fits";
   
   // Add a scope to lkimit the lifespan of the LikelihoodHandler
   while (true) {
@@ -95,16 +108,8 @@ BOOST_FIXTURE_TEST_CASE(test_PosteriorGridZ, LikelihoodFixture) {
 	
 	
 	  // Given
-	  std::vector<double>                   zs{0.0, 1.};
-	  std::vector<double>                   ebvs{0.0, 1.};
-	  std::vector<Euclid::XYDataset::QualifiedName> reddeing_curves{{"red_curve1"}, {"red_curve2"}, {"red_curve3"}};
-	  std::vector<Euclid::XYDataset::QualifiedName> seds{{"sed1"}, {"sed2"}, {"sed3"}};
-	  Euclid::PhzDataModel::ModelAxesTuple          axes = Euclid::PhzDataModel::createAxesTuple(zs, ebvs, reddeing_curves, seds);
-	  
-	  Euclid::PhzDataModel::RegionResults results{}; 
-	  
-	
-	  
+
+
 	  
 	  Euclid::PhzDataModel::DoubleGrid& posterior_grid = results.set<RegionResultType::POSTERIOR_LOG_GRID>(axes);
 	  for (auto &cell: posterior_grid) {
@@ -128,13 +133,11 @@ BOOST_FIXTURE_TEST_CASE(test_PosteriorGridZ, LikelihoodFixture) {
 	   break;
   }
     
-  std::string f_2_name = output_path.path().generic_string() +"/Sample_File_posterior_2.fits";
   std::ifstream f_2(f_2_name.c_str());
   BOOST_CHECK(f_2.good());
   
  
   
-  std::string f_3_name = output_path.path().generic_string() +"/Sample_File_posterior_3.fits";
   std::ifstream f_3(f_3_name.c_str());
   BOOST_CHECK(f_3.good());
   
@@ -182,6 +185,115 @@ BOOST_FIXTURE_TEST_CASE(test_PosteriorGridZ, LikelihoodFixture) {
   result.column(1).read(str_data, 1, 20);
   BOOST_CHECK_EQUAL(str_data[0], "ID_1");
   BOOST_CHECK_EQUAL(str_data[10], "ID_002");
+
+
+}
+
+
+//-----------------------------------------------------------------------------BOOST_FIXTURE_TEST_CASE(test_PosteriorGridZ, LikelihoodFixture) {
+ 
+BOOST_FIXTURE_TEST_CASE(test_PosteriorGridZ_int, LikelihoodFixture) { 
+  Source source_i_1{999,{}};
+  Source source_i_2{1000,{}};
+  Source source_i_3{1001,{}};
+  Source source_i_4{1002,{}};
+  Source source_i_5{1003,{}};
+
+  
+  // Add a scope to lkimit the lifespan of the LikelihoodHandler
+  while (true) {
+	  // Given
+	   std::map<std::string, std::map<std::string, std::tuple<double, double, std::string>>> param_config {};
+	  LikelihoodHandler<Euclid::PhzDataModel::RegionResultType::POSTERIOR_LOG_GRID, MockSampler> likelihoodHandler(output_path.path(), param_config, true, 10, 2);
+
+
+	  
+	
+	  
+	  
+	  Euclid::PhzDataModel::DoubleGrid& posterior_grid = results.set<RegionResultType::POSTERIOR_LOG_GRID>(axes);
+	  for (auto &cell: posterior_grid) {
+	  	cell=0.001;
+	  }
+	
+	  std::map<std::string, RegionResults> r_r;
+	  r_r["default"] = results;
+	  
+	  Euclid::PhzDataModel::SourceResults sr_1{};
+	  
+	  sr_1.set<Euclid::PhzDataModel::SourceResultType::REGION_RESULTS_MAP>(r_r);
+	
+	  
+	  // WHEN
+	  likelihoodHandler.handleSourceOutput(source_i_1, sr_1);
+	  likelihoodHandler.handleSourceOutput(source_i_2, sr_1);
+	  likelihoodHandler.handleSourceOutput(source_i_3, sr_1);
+	  likelihoodHandler.handleSourceOutput(source_i_4, sr_1);
+	  likelihoodHandler.handleSourceOutput(source_i_5, sr_1);
+	   break;
+  }
+  
+  // THEN
+
+  std::ifstream f_i(i_name.c_str());
+  BOOST_CHECK(f_i.good());
+  
+  std::ifstream f_s(f_1_name.c_str());
+  BOOST_CHECK(f_s.good());
+	
+    
+  std::ifstream f_2(f_2_name.c_str());
+  BOOST_CHECK(f_2.good());
+  
+ 
+  
+  std::ifstream f_3(f_3_name.c_str());
+  BOOST_CHECK(f_3.good());
+  
+  // THEN
+  CCfits::FITS fits_i{i_name, CCfits::RWmode::Read};
+  auto&        result_i = fits_i.extension(1);
+  result_i.readAllKeys();
+ 
+  BOOST_CHECK_EQUAL(result_i.rows(), 5);
+  BOOST_CHECK_EQUAL(result_i.numCols(), 2);
+
+  BOOST_CHECK_EQUAL(result_i.column(1).name(), "OBJECT_ID");
+  BOOST_CHECK_EQUAL(result_i.column(2).name(), "FILE_NAME");
+
+  BOOST_CHECK_EQUAL(result_i.column(1).format(), "K");
+  BOOST_CHECK_EQUAL(result_i.column(2).format(), "28A");
+  
+  
+  
+  // THEN
+  CCfits::FITS fits{f_1_name, CCfits::RWmode::Read};
+  auto&        result = fits.extension(1);
+  result.readAllKeys();
+ 
+  BOOST_CHECK_EQUAL(result.rows(), 20);
+  BOOST_CHECK_EQUAL(result.numCols(), 7);
+
+  BOOST_CHECK_EQUAL(result.column(1).name(), "OBJECT_ID");
+  BOOST_CHECK_EQUAL(result.column(2).name(), "GRID_REGION_INDEX");
+  BOOST_CHECK_EQUAL(result.column(3).name(), "SED_INDEX");
+  BOOST_CHECK_EQUAL(result.column(4).name(), "REDSHIFT");
+  BOOST_CHECK_EQUAL(result.column(5).name(), "RED_CURVE_INDEX");
+  BOOST_CHECK_EQUAL(result.column(6).name(), "EB_V");
+  BOOST_CHECK_EQUAL(result.column(7).name(), "LUMINOSITY");
+
+  BOOST_CHECK_EQUAL(result.column(1).format(), "K");
+  BOOST_CHECK_EQUAL(result.column(2).format(), "J");
+  BOOST_CHECK_EQUAL(result.column(3).format(), "J");
+  BOOST_CHECK_EQUAL(result.column(4).format(), "E");
+  BOOST_CHECK_EQUAL(result.column(5).format(), "J");
+  BOOST_CHECK_EQUAL(result.column(6).format(), "E");
+  BOOST_CHECK_EQUAL(result.column(7).format(), "E");
+  
+  std::vector<long> l_data{};
+  result.column(1).read(l_data, 1, 20);
+  BOOST_CHECK_EQUAL(l_data[0], 999);
+  BOOST_CHECK_EQUAL(l_data[10], 1000);
 
 
 }
