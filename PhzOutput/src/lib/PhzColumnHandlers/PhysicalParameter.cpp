f55c0fdbc4484bef9ef8a58139aa11f72d997979
DubathF <florian.dubath@unige.ch>
2024-09-13 12:00:54 +0200

    Add a loging when PP computation get wrong

--- PhzOutput/src/lib/PhzColumnHandlers/PhysicalParameter.cpp
+++ PhzOutput/src/lib/PhzColumnHandlers/PhysicalParameter.cpp
@@ -59,9 +59,14 @@ std::vector<Table::Row::cell_type> PhysicalParameter::convertResults(const Sourc
   }
 
   std::vector<Table::Row::cell_type> res_list{};
-  for (auto iter = m_param_config.cbegin(); iter != m_param_config.cend(); ++iter) {
-    const auto& funct_param = iter->second.at(sed);
-    res_list.push_back((funct_param).apply(scale * correction_factor));
+  try {
+    for (auto iter = m_param_config.cbegin(); iter != m_param_config.cend(); ++iter) {
+      const auto& funct_param = iter->second.at(sed);
+      res_list.push_back((funct_param).apply(scale * correction_factor));
+    }
+  } catch (const std::exception& e) {
+    logger.error() << "Error in the PP computation: check your PP config file (.fits)";
+    throw e;
   }
 
   return res_list;
