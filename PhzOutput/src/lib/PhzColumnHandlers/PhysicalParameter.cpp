f2b13410841db774699575c53ae903ea03a0e1ad
Florian <florian.dubath@unige.ch>
2026-06-19 09:35:23 +0200

    Bump version number and tentatively fix PP

--- PhzOutput/src/lib/PhzColumnHandlers/PhysicalParameter.cpp
+++ PhzOutput/src/lib/PhzColumnHandlers/PhysicalParameter.cpp
@@ -62,7 +62,7 @@ std::vector<Table::Row::cell_type> PhysicalParameter::convertResults(const Sourc
   try {
     for (auto iter = m_param_config.cbegin(); iter != m_param_config.cend(); ++iter) {
       const auto& funct_param = iter->second.at(sed);
-      res_list.push_back((funct_param).apply(scale * correction_factor));
+      res_list.push_back((funct_param).apply(scale / correction_factor));
     }
   } catch (const std::exception& e) {
     logger.error() << "Error in the PP computation: check your PP config file (.fits)";
